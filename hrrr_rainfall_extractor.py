6a609311c26b718dd6188a52ce99b8e05518d553
Felipe Quintero <fquintero@gmail.com>
2026-07-31 18:58:21 -0500

    Update hrrr_rainfall_extractor.py

--- hrrr_rainfall_extractor.py
+++ hrrr_rainfall_extractor.py
@@ -15,34 +15,34 @@ class HRRRRainfallExtractor:
         if from_npy==False:
             pass
 
-def fetch_from_npy(self, issue_time,start_time, end_time):
-    """
-    Finds the 24 hourly files for the given day using the exact naming convention,
-    extracts the values, and returns a matrix of shape (num_points, 24).
-    """
-    # Convert year and Day of Year to a string date (e.g., "20240101")
-    
-    nt = int((end_time - start_time) / 3600) + 1  # Number of time steps (hours)
-    # Pre-allocate the output matrix: (1 million points, 24 hours)
-    output = np.zeros((self.num_points, nt), dtype=np.float32)
-    
-    counter = 0
-    for i in range(start_time, end_time, 3600):
-        # 1. Build the two potential exact paths
-        filename = f"{issue_time}_{i}.py"
-        path_f = os.path.join(self.folder_path, filename)
-        if os.path.exists(path_f):    
-            try:
-                data = np.load(path_f)
-                output[:, counter] = data
-                counter += 1
-            except Exception as e:
-                print(f"Error reading file {filename}: {e}")
-        else:
-            print(f"File {path_f} not found")
-            counter +=1
-            
-    return output
+    def fetch_from_npy(self, issue_time,start_time, end_time):
+        """
+        Finds the 24 hourly files for the given day using the exact naming convention,
+        extracts the values, and returns a matrix of shape (num_points, 24).
+        """
+        # Convert year and Day of Year to a string date (e.g., "20240101")
+        
+        nt = int((end_time - start_time) / 3600) + 1  # Number of time steps (hours)
+        # Pre-allocate the output matrix: (1 million points, 24 hours)
+        output = np.zeros((self.num_points, nt), dtype=np.float32)
+        
+        counter = 0
+        for i in range(start_time, end_time, 3600):
+            # 1. Build the two potential exact paths
+            filename = f"{issue_time}_{i}.py"
+            path_f = os.path.join(self.folder_path, filename)
+            if os.path.exists(path_f):    
+                try:
+                    data = np.load(path_f)
+                    output[:, counter] = data
+                    counter += 1
+                except Exception as e:
+                    print(f"Error reading file {filename}: {e}")
+            else:
+                print(f"File {path_f} not found")
+                counter +=1
+                
+        return output
 
 # --- Usage Example ---
 if __name__ == '__main__':
