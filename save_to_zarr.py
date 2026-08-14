e020b34403195d2bb4c10a79f45325a742cb4e10
Felipe Quintero <fquintero@gmail.com>
2026-08-14 15:56:05 -0500

    Update save_to_zarr.py

--- save_to_zarr.py
+++ save_to_zarr.py
@@ -35,14 +35,14 @@ def create_empty_file(links = None,mode='state',year=2000,path ='/Dedicated/IFC/
     if mode in ['forecast_timeseries','forecasts_timeseries']:
         vars = ['routing_output']
         shape = (n_links,nt,5*24)
-        chunks = (1,1,5*24) # read forecast for 1 link , 1 issue time, five days
-        shards = (1000,24,5*24) 
+        chunks = (1000,1,5*24) # read forecast for 1 link , 1 issue time, five days
+        shards = (100000,24,5*24) 
         fileout=Path(path,f'forecast_timeseries_{year}.zarr')
     if mode in ['forecast_maps','forecasts_maps']:
         vars = ['routing_output']
         shape = (n_links,nt,5*24)
         chunks = (n_links,1,1)
-        shards = (1000,24,5*24)
+        shards = (n_links,24,5*24)
         fileout=Path(path,f'forecast_maps_{year}.zarr')
 
     if os.path.exists(fileout):  # Check if file already exists
@@ -104,23 +104,27 @@ def _write2(states,links,issuetime,p,year,mode,path):
     if not os.path.exists(p): 
         create_empty_file(links = links,mode=mode,year=year,path =path)   
     z = zarr.open(p)
-    indices = np.where(np.isin(z['issuetime'], issuetime))[0]
+    indices = np.where(np.isin(z['issuetime'], issuetime))[0][0]
+    #print('indices',indices)
+
     for var in states:
         if var not in z:
             raise ValueError(f"Variable '{var}' not found in the Zarr file.")
-        z[var][:, indices,0:5*24] = states[var]
+        #print(z[var][:, indices,:].shape)
+        #print(states[var][:,:].shape)
+        z[var][:, indices,:] = states[var][:,:]
 
 def write_forecast_timeseries(states,links,issuetime,path):
-    year = datetime.datetime.fromtimestamp(issuetime[0], tz=timezone.utc).year
+    year = datetime.datetime.fromtimestamp(issuetime, tz=timezone.utc).year
     p = Path(path,f'forecast_timeseries_{year}.zarr')
     mode='forecast_timeseries'
     _write2(states,links,issuetime,p,year,mode,path)
     
 
 def write_forecast_maps(states,links,issuetime,path):
-    year = datetime.datetime.fromtimestamp(issuetime[0], tz=timezone.utc).year
+    year = datetime.datetime.fromtimestamp(issuetime, tz=timezone.utc).year
     p = Path(path,f'forecast_maps_{year}.zarr')
-    mode='forecast_timeseries'
+    mode='forecast_maps'
     _write2(states,links,issuetime,p,year,mode,path)
 
 if __name__ == '__main__':
